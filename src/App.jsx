f0d704b79d3699aa5ef11662c9703fdcebdeb13e
Naqui Hasan <naqui@wrightresearch.in>
2025-09-13 14:05:38 +0530

    add redirection

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route , useNavigate} from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
 
